b8a1e3171e7ea004f38056ec825bf610c48795c9
Daniel Olson <emaildano@gmail.com>
2017-06-17 19:50:11 -0400

    pattern style helpers

--- assets/js/modules/commonScripts.js
+++ assets/js/modules/commonScripts.js
@@ -6,4 +6,15 @@ module.exports = function() {
 
   console.log('We have liftoff.');
 
+  $('.pattern--item').each(function( index ) {
+
+    var $this = $($this);
+    var $parent = $(this).parent();
+    var $children = $(this).children()
+    var $clone = $($children).clone();
+
+    $clone.appendTo($parent).wrapAll("<pre><code class='language-markup'><script type='prism-html-markup'></script></code></pre>");
+
+  });
+
 };
